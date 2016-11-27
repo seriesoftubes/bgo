453e3ad4b5f2535496aa1a64f0e5664ea7f26e2c
Brian Weidenbaum <brianweiden@gmail.com>
2016-11-27 11:13:15 -0800

    go fmt

--- game/board_test.go
+++ game/board_test.go
@@ -115,61 +115,61 @@ func TestLegalMovesTakeOverEnemy(t *testing.T) {
 
 // TestLegalMovesBearOff tests getting legal moves when one move can involve bearing off.
 func TestLegalMovesBearOff(t *testing.T) {
-  /* Board looks like:
-   x  w  v  u  t  s     r  q  p  o  n  m
-  =======================================
-   -  -  -  -  -  X |m| -  X  -  -  -  -
-                  X |m|    X
-                  X |m|    X
-                  X |m|
-                  X |m|
-                    |m|
+	/* Board looks like:
+	 x  w  v  u  t  s     r  q  p  o  n  m
+	=======================================
+	 -  -  -  -  -  X |m| -  X  -  -  -  -
+	                X |m|    X
+	                X |m|    X
+	                X |m|
+	                X |m|
+	                  |m|
 
 
-                    |w|
-               O  O |w|                X
-               O  O |w|                X
-               O  O |w|                X
-   X     O  O  O  O |w|                X
-   X  O  O  O  O  O |w| -  -  -  -  -  X
-  =======================================
-   a  b  c  d  e  f     g  h  i  j  k  l
-  */
-  // PCC == "X", PC = O
-  cases := []struct {
-    player *Player
-    diceAmt     uint8
-    wantLetters []string
-  }{
-    {PCC, 1, []string{"a", "l", "q", "s"}},
-    {PCC, 2, []string{"l", "q", "s"}},
-    {PCC, 3, []string{"l", "q", "s"}},
-    {PCC, 4, []string{"l", "q", "s"}},
-    {PCC, 5, []string{"l", "q", "s"}},
-    {PCC, 6, []string{"a", "l", "q"}},
+	                  |w|
+	             O  O |w|                X
+	             O  O |w|                X
+	             O  O |w|                X
+	 X     O  O  O  O |w|                X
+	 X  O  O  O  O  O |w| -  -  -  -  -  X
+	=======================================
+	 a  b  c  d  e  f     g  h  i  j  k  l
+	*/
+	// PCC == "X", PC = O
+	cases := []struct {
+		player      *Player
+		diceAmt     uint8
+		wantLetters []string
+	}{
+		{PCC, 1, []string{"a", "l", "q", "s"}},
+		{PCC, 2, []string{"l", "q", "s"}},
+		{PCC, 3, []string{"l", "q", "s"}},
+		{PCC, 4, []string{"l", "q", "s"}},
+		{PCC, 5, []string{"l", "q", "s"}},
+		{PCC, 6, []string{"a", "l", "q"}},
 
-    // TODO: this behavior is wrong. it should only allow bearing off when everything behind it has been beared off already.
-    {PC, 1, []string{"c", "d", "e", "f"}},
-    {PC, 2, []string{"b", "d", "e", "f"}},
-    {PC, 3, []string{"b", "c", "e", "f"}},
-    {PC, 4, []string{"b", "c", "d", "f"}},
-    {PC, 5, []string{"b", "c", "d", "e"}},
-    {PC, 6, []string{"b", "c", "d", "e", "f"}},
-  }
-  for _, c := range cases {
-    b := Board{}
-    b.Points = [NUM_BOARD_POINTS]*BoardPoint{
-      // counter-clockwise player is in bottom-left.
-      {PCC, 2}, {PC, 1}, {PC, 2}, {PC, 2}, {PC, 5}, {PC, 5}, {}, {}, {}, {}, {}, {PCC, 5},
-      {}, {}, {}, {}, {PCC, 3}, {}, {PCC, 5}, {}, {}, {}, {}, {},
-      //                                                        clockwise player in top-left.
-    }
+		// TODO: this behavior is wrong. it should only allow bearing off when everything behind it has been beared off already.
+		{PC, 1, []string{"c", "d", "e", "f"}},
+		{PC, 2, []string{"b", "d", "e", "f"}},
+		{PC, 3, []string{"b", "c", "e", "f"}},
+		{PC, 4, []string{"b", "c", "d", "f"}},
+		{PC, 5, []string{"b", "c", "d", "e"}},
+		{PC, 6, []string{"b", "c", "d", "e", "f"}},
+	}
+	for _, c := range cases {
+		b := Board{}
+		b.Points = [NUM_BOARD_POINTS]*BoardPoint{
+			// counter-clockwise player is in bottom-left.
+			{PCC, 2}, {PC, 1}, {PC, 2}, {PC, 2}, {PC, 5}, {PC, 5}, {}, {}, {}, {}, {}, {PCC, 5},
+			{}, {}, {}, {}, {PCC, 3}, {}, {PCC, 5}, {}, {}, {}, {}, {},
+			//                                                        clockwise player in top-left.
+		}
 
-    gotLetters := mLetters(b.LegalMoves(c.player, c.diceAmt))
-    if !reflect.DeepEqual(gotLetters, c.wantLetters) {
-      t.Errorf("LegalMoves for diceAmt: %v unexpected. got %v want %v", c.diceAmt, gotLetters, c.wantLetters)
-    }
-  }
+		gotLetters := mLetters(b.LegalMoves(c.player, c.diceAmt))
+		if !reflect.DeepEqual(gotLetters, c.wantLetters) {
+			t.Errorf("LegalMoves for diceAmt: %v unexpected. got %v want %v", c.diceAmt, gotLetters, c.wantLetters)
+		}
+	}
 }
 
 func mLetters(moves []*Move) []string {
