65a05708dc036463697c4eec7a5f50d6fbe703e1
Brian Weidenbaum <brianweiden@gmail.com>
2016-12-05 02:40:17 -0800

    better logging during training

--- main.go
+++ main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/seriesoftubes/bgo/ctrl"
 )
 
+const trainings = 1000
+
 func main() {
 	ctrl := ctrl.New(false)
 
-	for i := 0; i < 1000; i++ {
-		if i%10 == 0 {
-			fmt.Println("trained on", i, "games")
+	start := time.Now()
+	for i := 0; i < trainings; i++ {
+		if i%1000 == 0 {
+			fmt.Println(time.Now(), "trained on", i, "games")
 		}
 		ctrl.PlayOneGame(0, false)
 	}
+	fmt.Println("trained", trainings, "times in", time.Since(start))
 
 	ctrl.PlayOneGame(1, true)
 }
