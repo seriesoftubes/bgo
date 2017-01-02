52f59716bd6e77c5c3b1718d458f45a1b11acf1e
Brian Weidenbaum <brianweiden@gmail.com>
2017-01-01 22:16:12 -0800

    refactor

--- main.go
+++ main.go
@@ -36,104 +36,34 @@ var (
 	outFilePathPtr      = flag.String("config_outfile", "", "The file that will contain the updated neural net config")
 )
 
-var (
-	learningRateReducerInterval     = uint64(42000)
-	learningRateReductionMultiplier = float32(0.5)
-	lrConfigMu                      sync.RWMutex
-	gamesPlayed                     uint64
+type (
+	learningRateManager struct {
+		sync.RWMutex
+		interval   uint64  // Every `interval` games, we update the learningRate
+		multiplier float32 // Every `interval` games, we multiply the learningRate by `multiplier`
+	}
+
+	// Gotta train'em all! Poke-model!
+	pokemodelTrainer struct {
+		configInFile, configOutFile, varianceLogsFilePath string
+		hasLoadedNN                                       bool
+		startGamesPlayed, gamesPlayed                     uint64
+		lrManager                                         *learningRateManager
+	}
 )
 
-func loadNeuralNetwork(filePath string) {
-	fmt.Println("loading neural network config from", filePath)
-
-	f, err := os.Open(filePath)
-	if err != nil {
-		fmt.Printf("could not open neural network file %q. Skipping. %v\n", filePath, err)
-		return
-	}
-	defer f.Close()
-
-	if existingGamesPlayed, err := nnet.Load(f); err != nil {
-		panic("could not deserialize neural network: " + err.Error())
-	} else {
-		atomic.StoreUint64(&gamesPlayed, existingGamesPlayed)
+func newTrainer(configInFile, configOutFile string) *pokemodelTrainer {
+	if !strings.HasSuffix(configInFile, ".json") || !strings.HasSuffix(configOutFile, ".json") {
+		panic("both the infile and outfile must have a .json suffix.")
 	}
 
-	fmt.Println("neural net loaded!")
-}
-
-func saveNeuralNetwork(filePath string, waitForWrites bool) {
-	fmt.Println("saving neural net config to", filePath)
-
-	f, err := os.Create(filePath) // always overwrites the existing file.
-	if err != nil {
-		panic("could not create file: " + err.Error())
+	varianceLogsFilePath := strings.Replace(configOutFile, ".json", "_variance_report.txt", 1)
+	return &pokemodelTrainer{
+		configInFile:         configInFile,
+		configOutFile:        configOutFile,
+		varianceLogsFilePath: varianceLogsFilePath,
+		lrManager:            &learningRateManager{interval: 42000, multiplier: 0.00001},
 	}
-	defer f.Close()
-
-	if err := nnet.Save(f, atomic.LoadUint64(&gamesPlayed), waitForWrites); err != nil {
-		panic("couldnt save neural network: " + err.Error())
-	}
-
-	fmt.Println("neural net config saved!")
-}
-
-func incrementGamesPlayed() {
-	atomic.AddUint64(&gamesPlayed, 1)
-	ct := atomic.LoadUint64(&gamesPlayed)
-
-	if ct%500 == 0 {
-		fmt.Println(time.Now(), "trained on", ct, "games")
-	}
-
-	lrConfigMu.RLock()
-	my_interval, my_multiplier := learningRateReducerInterval, learningRateReductionMultiplier
-	lrConfigMu.RUnlock()
-
-	if ct%my_interval == 0 {
-		fmt.Println("multiplying the neural net's learning rate by", my_multiplier)
-		nnet.MultiplyLearningRate(my_multiplier)
-	}
-}
-
-func filePathFromFlag(fp *string) string {
-	if fp == nil || *fp == "" {
-		u, err := user.Current()
-		if err != nil {
-			panic("could not get current OS user: " + err.Error())
-		}
-		return fmt.Sprintf("%s/Desktop/bgo_nnet.json", u.HomeDir)
-	}
-
-	return *fp
-}
-
-func writeVarianceLogs(startGamesPlayed uint64, filePath string, waitForWrites bool) {
-	fmt.Println("saving variance data to", filePath)
-
-	f, err := os.Create(filePath) // always overwrites the existing file.
-	if err != nil {
-		panic("could not create file: " + err.Error())
-	}
-	defer f.Close()
-	w := bufio.NewWriter(f)
-
-	writeLine := func(ln string) {
-		if _, err = w.WriteString(ln); err != nil {
-			panic(fmt.Sprintf("w.WriteString(%q) error: %v", ln, err))
-		}
-	}
-
-	writeLine("GamesPlayed\tAvgVariance\n")
-	for i, v := range nnperf.GameAverageVariances(-1, waitForWrites) {
-		writeLine(fmt.Sprintf("%v\t%v\n", uint64(i)+1+startGamesPlayed, v))
-		if i%1000 == 0 {
-			w.Flush()
-		}
-	}
-	w.Flush()
-
-	fmt.Println("done saving variance data!")
 }
 
 func onHelpCmd() {
@@ -146,6 +76,17 @@ func onHelpCmd() {
 	fmt.Println(cmdprefixChangeLearningRateReducerInterval, "(plus a number like bla_123123)")
 	fmt.Println(cmdprefixChangeLearningRateReducerMultiplier, "(plus a number like bla_0.8")
 	fmt.Println(cmdHelp)
+}
+
+func onMulrCmd(cmd string) {
+	factor, err := float32FromCommand(cmd)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("multiplying learning rate by", factor)
+	nnet.MultiplyLearningRate(factor)
 }
 
 func float32FromCommand(cmd string) (float32, error) {
@@ -162,54 +103,157 @@ func float32FromCommand(cmd string) (float32, error) {
 	return float32(factor), nil
 }
 
-func onMulrCmd(cmd string) {
-	factor, err := float32FromCommand(cmd)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
+func filePathFromFlag(fp *string) string {
+	if fp == nil || *fp == "" {
+		u, err := user.Current()
+		if err != nil {
+			panic("could not get current OS user: " + err.Error())
+		}
+		return fmt.Sprintf("%s/Desktop/bgo_nnet.json", u.HomeDir)
 	}
 
-	fmt.Println("multiplying learning rate by", factor)
-	nnet.MultiplyLearningRate(factor)
+	return *fp
 }
 
-func onChangeLearningRateReducerIntervalCmd(cmd string) {
-	defer lrConfigMu.Unlock()
-	lrConfigMu.Lock()
+func (lrm *learningRateManager) maybeChangeLearningRate(numGamesCompleted uint64) {
+	if my_multiplier, my_interval := lrm.params(); numGamesCompleted%my_interval == 0 {
+		fmt.Println("multiplying the neural net's learning rate by", my_multiplier)
+		nnet.MultiplyLearningRate(my_multiplier)
+	}
+}
 
+func (lrm *learningRateManager) setInterval(newInterval uint64) {
+	defer lrm.Unlock()
+	lrm.Lock()
+
+	fmt.Println("setting learningRateReducerInterval to", newInterval)
+	lrm.interval = newInterval
+}
+
+func (lrm *learningRateManager) setMultiplier(newMultiplier float32) {
+	defer lrm.Unlock()
+	lrm.Lock()
+
+	fmt.Println("setting learningRateReductionMultiplier to", newMultiplier)
+	lrm.multiplier = newMultiplier
+}
+
+func (lrm *learningRateManager) params() (float32, uint64) {
+	defer lrm.RUnlock()
+	lrm.RLock()
+	return lrm.multiplier, lrm.interval
+}
+
+func (pt *pokemodelTrainer) onCfgCmd() {
+	learningRate, decayRate := nnet.LearningParams()
+	multiplier, interval := pt.lrManager.params()
+
+	fmt.Println("learningRate", learningRate)
+	fmt.Println("decayRate", decayRate)
+	fmt.Println("learningRateReductionMultiplier", multiplier)
+	fmt.Println("learningRateReductionInterval", interval)
+	fmt.Println("gamesPlayed", atomic.LoadUint64(&pt.gamesPlayed))
+}
+
+func (pt *pokemodelTrainer) onGameCompleted() {
+	atomic.AddUint64(&pt.gamesPlayed, 1)
+	ct := atomic.LoadUint64(&pt.gamesPlayed)
+	if ct%500 == 0 {
+		fmt.Println(time.Now(), "trained on", ct, "games")
+	}
+	pt.lrManager.maybeChangeLearningRate(ct)
+}
+
+func (pt *pokemodelTrainer) loadNeuralNetwork() {
+	filePath := pt.configInFile
+	fmt.Println("loading neural network config from", filePath)
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("could not open neural network file %q. Skipping. %v\n", filePath, err)
+		pt.hasLoadedNN = true
+		return
+	}
+	defer f.Close()
+
+	if existingGamesPlayed, err := nnet.Load(f); err != nil {
+		panic("could not deserialize neural network: " + err.Error())
+	} else {
+		atomic.StoreUint64(&pt.gamesPlayed, existingGamesPlayed)
+		atomic.StoreUint64(&pt.startGamesPlayed, existingGamesPlayed)
+	}
+
+	pt.hasLoadedNN = true
+	fmt.Println("neural net loaded!")
+}
+
+func (pt *pokemodelTrainer) saveNeuralNetwork(waitForWrites bool) {
+	filePath := pt.configOutFile
+	fmt.Println("saving neural net config to", filePath)
+
+	f, err := os.Create(filePath) // always overwrites the existing file.
+	if err != nil {
+		panic("could not create file: " + err.Error())
+	}
+	defer f.Close()
+
+	if err := nnet.Save(f, atomic.LoadUint64(&pt.gamesPlayed), waitForWrites); err != nil {
+		panic("couldnt save neural network: " + err.Error())
+	}
+
+	fmt.Println("neural net config saved!")
+}
+
+func (pt *pokemodelTrainer) writeVarianceLogs(waitForWrites bool) {
+	filePath := pt.varianceLogsFilePath
+	fmt.Println("saving variance data to", filePath)
+
+	f, err := os.Create(filePath) // always overwrites the existing file.
+	if err != nil {
+		panic("could not create file: " + err.Error())
+	}
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+	writeLine := func(ln string) {
+		if _, err = w.WriteString(ln); err != nil {
+			panic(fmt.Sprintf("w.WriteString(%q) error: %v", ln, err))
+		}
+	}
+
+	writeLine("GamesPlayed\tAvgVariance\n")
+	startGamesPlayed := atomic.LoadUint64(&pt.startGamesPlayed)
+	for i, v := range nnperf.GameAverageVariances(-1, waitForWrites) {
+		writeLine(fmt.Sprintf("%v\t%v\n", uint64(i)+1+startGamesPlayed, v))
+		if i%1000 == 0 {
+			w.Flush()
+		}
+	}
+	w.Flush()
+
+	fmt.Println("done saving variance data!")
+}
+
+func (pt *pokemodelTrainer) onChangeLearningRateReducerIntervalCmd(cmd string) {
 	newInterval, err := float32FromCommand(cmd)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println("could not parse number from cmd", cmd, err.Error())
 		return
 	}
-
-	fmt.Println("setting learningRateReducerInterval to", uint64(newInterval))
-	learningRateReducerInterval = uint64(newInterval)
+	pt.lrManager.setInterval(uint64(newInterval))
 }
 
-func onChangeLearningRateReducerMultiplierCmd(cmd string) {
-	defer lrConfigMu.Unlock()
-	lrConfigMu.Lock()
-
+func (pt *pokemodelTrainer) onChangeLearningRateReducerMultiplierCmd(cmd string) {
 	newMultiplier, err := float32FromCommand(cmd)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-
-	fmt.Println("setting learningRateReductionMultiplier to", newMultiplier)
-	learningRateReductionMultiplier = newMultiplier
+	pt.lrManager.setMultiplier(newMultiplier)
 }
 
-func onCfgCmd() {
-	learningRate, decayRate := nnet.LearningParams()
-	fmt.Println("learningRate", learningRate)
-	fmt.Println("decayRate", decayRate)
-	fmt.Println("learningRateReductionMultiplier", learningRateReductionMultiplier)
-	fmt.Println("learningRateReducerInterval", learningRateReducerInterval)
-}
-
-func readCommands(doneChan chan bool) {
+// TODO: split this into its own struct.
+func (pt *pokemodelTrainer) readCommands(doneChan chan bool) {
 	var previousCmd string
 
 	for {
@@ -240,13 +284,13 @@ func readCommands(doneChan chan bool) {
 				fmt.Println(v)
 			}
 		} else if cmd == cmdCFG {
-			onCfgCmd()
+			pt.onCfgCmd()
 		} else if strings.HasPrefix(cmd, cmdprefixMultiplyLearningRate) {
 			onMulrCmd(cmd)
 		} else if strings.HasPrefix(cmd, cmdprefixChangeLearningRateReducerInterval) {
-			onChangeLearningRateReducerIntervalCmd(cmd)
+			pt.onChangeLearningRateReducerIntervalCmd(cmd)
 		} else if strings.HasPrefix(cmd, cmdprefixChangeLearningRateReducerMultiplier) {
-			onChangeLearningRateReducerMultiplierCmd(cmd)
+			pt.onChangeLearningRateReducerMultiplierCmd(cmd)
 		} else if cmd == cmdHelp {
 			onHelpCmd()
 		}
@@ -257,47 +301,47 @@ func readCommands(doneChan chan bool) {
 	}
 }
 
-func main() {
-	flag.Parse()
-
-	infilePath := filePathFromFlag(inFilePathPtr)
-	outfilePath := filePathFromFlag(outFilePathPtr)
-	if !strings.HasSuffix(infilePath, ".json") || !strings.HasSuffix(outfilePath, ".json") {
-		panic("both the infile and outfile must have a .json suffix.")
+func (pt *pokemodelTrainer) train(numGoroutines, gamesToPlayPerGoroutine uint64) {
+	if !pt.hasLoadedNN {
+		pt.loadNeuralNetwork()
 	}
-	varianceLogsFilePath := strings.Replace(outfilePath, ".json", "_variance_report.txt", 1)
 
-	gamesToPlayPerGoroutine := *totalGamesToPlayPtr / *numGoroutinesPtr
-	fmt.Printf("training on %d games (%d goroutines X %d games per goroutine)...\n", *numGoroutinesPtr*gamesToPlayPerGoroutine, *numGoroutinesPtr, gamesToPlayPerGoroutine)
-
-	loadNeuralNetwork(infilePath)
-	doneChan := make(chan bool, 1)
-	go readCommands(doneChan)
+	fmt.Printf("training on %d games (%d goroutines X %d games per goroutine)...\n", numGoroutines*gamesToPlayPerGoroutine, numGoroutines, gamesToPlayPerGoroutine)
 	start := time.Now()
-	startGamesPlayed := gamesPlayed
+
+	doneChan := make(chan bool, 1)
+	go pt.readCommands(doneChan)
 
 	var wg sync.WaitGroup
-	for i := uint64(0); i < *numGoroutinesPtr; i++ {
+	for i := uint64(0); i < numGoroutines; i++ {
 		wg.Add(1)
+
 		go func() {
 			mgr := ctrl.New(false)
 			for j := uint64(0); j < gamesToPlayPerGoroutine; j++ {
 				mgr.PlayOneGame(0, false) // Play 1 game with 0 humans and don't stop learning!
 				mgr.TransmitStatsFromMostRecentGame()
-				incrementGamesPlayed()
+				pt.onGameCompleted()
 			}
 			mgr.WaitForStats()
 			wg.Done()
 		}()
 	}
 	wg.Wait()
+
 	doneChan <- true
 	close(doneChan)
-	fmt.Println("trained", gamesPlayed-startGamesPlayed, "times in", time.Since(start))
+	fmt.Printf("trained %d times in %v\n", atomic.LoadUint64(&pt.gamesPlayed)-atomic.LoadUint64(&pt.startGamesPlayed), time.Since(start))
+}
 
-	waitForWrites := true
-	saveNeuralNetwork(outfilePath, waitForWrites)
-	writeVarianceLogs(startGamesPlayed, varianceLogsFilePath, waitForWrites)
+func main() {
+	flag.Parse()
+
+	trainer := newTrainer(filePathFromFlag(inFilePathPtr), filePathFromFlag(outFilePathPtr))
+	trainer.loadNeuralNetwork()
+	trainer.train(*numGoroutinesPtr, *totalGamesToPlayPtr / *numGoroutinesPtr)
+	trainer.saveNeuralNetwork(true /* waitForWrites=true*/)
+	trainer.writeVarianceLogs(true /* waitForWrites=true*/)
 
 	mgr := ctrl.New(true /* debug=true*/)
 	mgr.PlayOneGame(1, true /* stopLearning=true */)
